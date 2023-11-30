33b01f09e66d122ed1f01cb0f681c50004b6e824
anhquoc98 <anhquoc98@gmail.com>
2023-11-30 10:24:49 +0700

    demo node

--- demo/server.js
+++ demo/server.js
@@ -11,11 +11,3 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-// http.createServer(function (req, res) {
-// fs.readFile('demofile1.html', function(err, data) {
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//    res.write(data);
-//     return res.end();
-//  });
-// }).listen(8080);
